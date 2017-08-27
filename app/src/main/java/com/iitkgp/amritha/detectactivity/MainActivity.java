7a3620a58462a4cfbdc4f190b9f60d3b3fa22ddc
AMRITHA <pvamritha@gmail.com>
2017-08-27 22:39:00 +0530

    Initial commit

---
+++ app/src/main/java/com/iitkgp/amritha/detectactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iitkgp.amritha.detectactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
